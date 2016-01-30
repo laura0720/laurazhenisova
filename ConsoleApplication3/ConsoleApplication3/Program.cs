7c7602b4975286b0d98827424da087be92954687
Laura <lauzhenisova@gmail.com>
2016-01-30 13:36:27 +0600

    hello worldt

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+        }
+    }
+}
