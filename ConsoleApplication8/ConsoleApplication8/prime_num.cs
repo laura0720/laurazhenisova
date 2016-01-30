4f25e3c8bc17e6c6ef47c5276c9f2163245092cf
Laura <lauzhenisova@gmail.com>
2016-01-30 21:02:13 +0600

    dobavleny comments

--- ConsoleApplication8/ConsoleApplication8/prime_num.cs
+++ ConsoleApplication8/ConsoleApplication8/prime_num.cs
@@ -10,19 +10,19 @@ namespace ConsoleApplication8
     {
         static void Main(string[] args)
         {
-            string[] a = Console.ReadLine().Split();
-            for (int i = 0; i < a.Length; i++)
+            string[] a = Console.ReadLine().Split(); // sozdanie string dlya vvedeniya chisel i dlya togo chtoby prochitat
+            for (int i = 0; i < a.Length; i++) //probegaet po stroke
             {
-                int is_prime = int.Parse(a[i]);
+                int is_prime = int.Parse(a[i]); //perevod is string v int
                 int sum = 0;
-                for (int j = 2; j < is_prime; j++)
+                for (int j = 2; j < is_prime; j++) //proverka so 2 chisla
                 {
-                    if (is_prime % j == 0)
-                        sum++;
+                    if (is_prime % j == 0) //esli chislo delitsya bez ostatka to kolvo prime chisel uvelichivaetsya
+                        sum++; //t.e prime eto chislo kotoroe delitsya na samu sebya i na 1
 
                 }
-                if (sum < 1)
-                    Console.WriteLine(is_prime);
+                if (sum < 1)//kolvo ne dolzhno byt' menshe 1
+                    Console.WriteLine(is_prime); // vyvod prostyh chisel
             }
             Console.ReadKey();
         }
