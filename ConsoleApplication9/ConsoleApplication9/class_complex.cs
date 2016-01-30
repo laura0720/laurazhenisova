d1434172303eb878a5d76745b8163d55d5d8dc71
Laura <lauzhenisova@gmail.com>
2016-01-30 21:47:35 +0600

    complex

---
+++ ConsoleApplication9/ConsoleApplication9/class_complex.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Complex
+    {
+        public int a, b;
+        public Complex(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public override string ToString()
+        {
+            return a + "*" + b;
+        }
+        public static Complex operator +(Complex a1, Complex a2)
+        {
+            Complex a3 = new Complex(a1.a * a2.a, a1.b * a2.b);
+            return a3;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex m = new Complex(12, 9);
+            Complex n = new Complex(12, 11);
+            Complex s = m + n;
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
