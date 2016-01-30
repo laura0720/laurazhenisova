43d24c274b2a47db8f5cf5264dbaff9a4a81466d
Laura <lauzhenisova@gmail.com>
2016-01-30 22:20:22 +0600

    student

---
+++ ConsoleApplication10/ConsoleApplication10/student_class.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Student //sozdanie classa student
+    {
+        public string Name;//dobavlenie metodov
+        public string Faculty;
+        public string ID;
+        public double GPA;
+        public Student(string Name, string Faculty, string ID, double GPA) //konstruktor
+        {
+            this.Name = Name;
+            this.Faculty = Faculty;
+            this.ID = ID;
+            this.GPA = GPA;
+        }
+        public override string ToString() //vyvod metodov
+        {
+            return Name + " " + Faculty + " " + ID + " " + GPA;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("Laura", "FIT", "15BD02028", 4.0); //zapolnenie
+            Console.WriteLine(s); //vyvod
+            Console.ReadKey();
+
+        }
+    }
+}
